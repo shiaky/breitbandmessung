d5e4ab06c677b53f1ed6b92d83ab7fab549baecd
Shnezzin <sebastian.kraehe@googlemail.com>
2021-12-20 16:46:48 +0100

    remove config, create merge

---
+++ merge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OutFileName="messergebnisse.csv"                       # Fix the output name
+i=0                                       # Reset a counter
+for filename in ./messprotokolle/*.csv; do 
+ if [ "$filename"  != "$OutFileName" ] ;      # Avoid recursion 
+ then 
+   if [[ $i -eq 0 ]] ; then 
+      head -1  "$filename" >   "$OutFileName" # Copy header if it is the first file
+   fi
+   tail -n +2  "$filename" >>  "$OutFileName" # Append from the 2nd line each file
+   echo "" >>  "$OutFileName"
+   i=$(( $i + 1 ))                            # Increase the counter
+ fi
+done
