62419f6b2750ba23a3925829a521431a14976373
shneezin <sebastian.kraehe@googlemail.com>
2021-12-31 00:07:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,21 @@ source config.shlib;
 TZ="$(config_get timezone)";
 CRON_SCHEDULE="$(config_get crontab)";
 RUN_ONCE="$(config_get run_once)";
+RUN_ON_STARTUP="$(config_get run_on_startup)";
 
 echo "Setting timezone: $TZ"
 rm -rf /etc/localtime
 ln -s /usr/share/zoneinfo/$TZ /etc/localtime
 
 
+echo "Run on startup: ${RUN_ON_STARTUP}"
+if [ "$RUN_ON_STARTUP" = "true" ]; then
+    /usr/local/bin/node /usr/src/app/index.js
+fi
+
 echo "Run once: ${RUN_ONCE}"
 if [ "$RUN_ONCE" = "false" ]; then
 printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
-/usr/local/bin/node /usr/src/app/index.js
 echo "Setting cron schedule: ${CRON_SCHEDULE}"
 echo "${CRON_SCHEDULE} /bin/bash -c '/usr/local/bin/node /usr/src/app/index.js' > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
 cron -f
