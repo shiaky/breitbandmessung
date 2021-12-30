be703aef5d958bb480332dd451933e141703edd7
shneezin <sebastian.kraehe@googlemail.com>
2021-12-31 00:42:16 +0100

    Update create.sh

--- create.sh
+++ create.sh
@@ -8,5 +8,4 @@ mkdir $PWD/config
 mkdir $PWD/messprotokolle
 chmod 777 $PWD/messprotokolle
 docker create -v $PWD/config/:/usr/src/app/config:rw -v $PWD/messprotokolle:/export/ --name "breitbandmessung" breitbandmessung:latest 
-#docker update --restart unless-stopped breitbandmessung
 docker start breitbandmessung
